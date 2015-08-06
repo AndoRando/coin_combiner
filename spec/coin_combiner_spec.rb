be6c18d20b828806384f5b88ef65b629fc0e7ae4
Andrew Brennwald & Yelena Nedelko <student@epicodus.com>
2015-08-06 16:26:24 -0700

    Add working spec for final output.

--- spec/coin_combiner_spec.rb
+++ spec/coin_combiner_spec.rb
@@ -6,7 +6,11 @@ describe("Fixnum#coin_combiner") do
   #   expect(55.coin_combiner()).to(eq(2))
   # end
 
-  it("accepts an amount of change and divides it into quarters and dimes") do
+  #it("accepts an amount of change and divides it into dimes and quarters")
+  #  expect(65.coin_combiner()).to(eq([2, 1]))
+  #end
+
+  it("accepts an amount of change and divides it into qs, ds, ns, and ps") do
     expect(66.coin_combiner()).to(eq([2, 1, 1, 1]))
   end
 end
